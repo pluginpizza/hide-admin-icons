71cf0cd428bd14e21715609f1612258ac3e5c4f7
Barry Ceelen <barry@multipop.org>
2024-07-26 19:53:48 +0200

    Minor refactor

--- README.md
+++ README.md
@@ -3,8 +3,14 @@ Hide Admin Icons
 
 Hides the WordPress admin navigation menu icons and admin page title icons.
 
-##WordPress plugin directory
+## Installation
 
-The plugin is also hosted on the [WordPress plugin directory](http://wordpress.org/plugins/hide-admin-icons/)
+Hide Admin Icons is hosted on the [WordPress plugin directory](https://wordpress.org/plugins/hide-admin-icons/) and can be installed via the WordPress dashboard.
 
+1. Visit the Plugins page within your WordPress dashboard and select ‘Add New’
+1. Search for 'Hide Admin Icons' and install the plugin
+1. Activate Bar Site ID from your Plugins page
 
+### Composer
+
+Hide Admin Icons can be added as a dependency to your project via the [wpackagist composer repository](https://wpackagist.org/search?q=hide-admin-icons).
--- fufi-hide-admin-icons.php
+++ fufi-hide-admin-icons.php
@@ -1,29 +1,31 @@
 <?php
-/*
-Plugin Name: Hide Admin Icons
-Plugin URI: http://wordpress.org/plugins/hide-admin-icons/
-Description: Hides the icons in the admin menu and page titles.
-Version: 1.4
-Author: Barry Ceelen
-Author URI: http://functionsfile.com
+/**
+ * Main plugin file
+ *
+ * Plugin Name: Hide Admin Icons
+ * Plugin URI: http://wordpress.org/plugins/hide-admin-icons/
+ * Description: Hides the icons in the admin menu and page titles.
+ * Version: 1.4
+ * Author: Barry Ceelen
+ * Author URI: http://functionsfile.com
+ *
+ * @package PluginPizza\HideAdminIcons
+ */
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
-
+// Enqueue stylesheet.
 add_action( 'admin_enqueue_scripts', 'fufi_hai_admin_enqueue_scripts' );
 
+/**
+ * Enqueue stylesheet.
+ *
+ * @return void
+ */
 function fufi_hai_admin_enqueue_scripts() {
-	wp_enqueue_style( 'fufi_hide_admin_icons', plugins_url( 'fufi-hide-admin-icons.css', __FILE__) );
+
+	wp_enqueue_style(
+		'fufi_hide_admin_icons',
+		plugins_url( 'fufi-hide-admin-icons.css', __FILE__ ),
+		array(),
+		'1.4'
+	);
 }
